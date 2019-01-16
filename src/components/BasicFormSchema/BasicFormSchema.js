fd4eacc5292b2fc10e095e7e92a3ef82f64c8116
Aleksandr Serov <sashaserov2010@gmail.com>
2019-01-16 09:28:45 +0300

    little fix regExp

--- src/components/BasicFormSchema/BasicFormSchema.js
+++ src/components/BasicFormSchema/BasicFormSchema.js
@@ -4,8 +4,12 @@ const BasicFormSchema = Yup.object().shape({
 	name: Yup.string()
 	.min(2,"Имя должно быть длинее 2 символов!")
 	.max(30, "Имя не может быть длиннее 30 символов!")
-	.matches(/[А-Яа-я]+/g, 'Имя должно быть на русском языке!'),
-	birthday: Yup.string().matches(/\d{2}\.\d{2}\.\d{4}/, "Заполните дату полностью!" ),
-	phone: Yup.string().matches(/\+7 \(\d{3}\) \d{3}-\d{4}/, "Заполните номер телефона полностью!" ),
+	.required('Поле должно быть заполнено!'),
+	birthday: Yup.string()
+	.matches(/\d{2}\.\d{2}\.\d{4}/, "Заполните дату полностью!" )
+	.required('Поле должно быть заполнено!'),
+	phone: Yup.string()
+	.matches(/\+7 \(\d{3}\) \d{3}-\d{4}/, "Заполните номер телефона полностью!" )
+	.required('Поле должно быть заполнено!'),
 });
 export default BasicFormSchema;
